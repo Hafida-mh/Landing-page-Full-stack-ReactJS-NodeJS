5a0c19e4c6a1243900145c9409120576afb846c9
Mechkour Hafida <79710349+Hafida-mh@users.noreply.github.com>
2023-02-01 19:40:01 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function App() {
-
 const [NomUser, setNomUser] = useState();
 const [PrenomUser, setPrenomUser] = useState();
 const [EmailUser, setEmailUser] = useState();
@@ -15,13 +14,9 @@ const [ActivityUser, setActivityUser] = useState();
 const [JobUser, setJobUser] = useState();
 const [TypeWebSiteUser, setTypeWebSiteUser] = useState();
 const [users, setUsers] = useState([""]);
-
-
-  const handleUserInfo = async (e) => {
+const handleUserInfo = async (e) => {
     e.preventDefault();
-
     /* hello*/
-
     try {
     if(NomUser && PrenomUser && EmailUser && TelUser && CorporateUser && ActivityUser && JobUser && TypeWebSiteUser ) {
        const { data } = await axios.post(`http://localhost:2000/user`, {
@@ -34,26 +29,17 @@ const [users, setUsers] = useState([""]);
         JobUser : JobUser,
         TypeWebSiteUser : TypeWebSiteUser
        });
-     
-       
     setUsers([...users,{
       NomUser : data.data.NomUser,
       PrenomUser : data.data.PrenomUser,
       EmailUser : data.data.EmailUser,
-
       TelUser : data.data.TelUser,
       CorporateUser : data.data.CorporateUser,
       ActivityUser : data.data.ActivityUser,
-
       JobUser : data.data.JobUser,
       TypeWebSiteUser : data.data.TypeWebSiteUser,
-
        }
-
     ])
-
-
-
     console.log(users);
     alert('welcome');
     } 
@@ -61,95 +47,61 @@ const [users, setUsers] = useState([""]);
       alert('veuillez remplir les champs vides');
     }
  } 
- 
- 
- 
  catch (error) {
      console.log(error);
  }
-
-
-
-
 }
-  
-
-
 
   return (
     <div className="App">
       <img src={Background} className="imgBack" />
-
-
-
-
-
       <div className="appContent">
         <div className="titleForm">
           <h1> REMPLISSEZ <section className="formulaire">  LE FORMULAIRE </section> <section className="suivant"> SUIVANT POUR  </section> <section className="recevoir">  RECEVOIR </section>  <section className="devis"> UN DEVIS </section>  </h1>
         </div>
 
         <div className="form">
-
           <div className="firstForm">
-
             <div className="form1">
-             <b> <p>  Nom   </p></b>
+              <b> <p>  Nom   </p> </b>
               <div className="inputField">  <input type="text" onChange={(e)=> setNomUser(e.target.value)} value={NomUser}/> </div>
             </div>
-
             <div className="form1">
-            <b>   <p>  Prénom  </p> </b>
+              <p>  <b>  Prénom  </p> </b>
               <div className="inputField">  <input type="text" onChange={(e)=> setPrenomUser(e.target.value)} value={PrenomUser} /> </div>
             </div>
-
             <hr className="line" />
           </div>
-
           <div className="secondForm">
-
             <div className="form2">
               <p> Email </p>
               <div className="inputField">  <input type="text" placeholder="entrer adresse mail" onChange={(e)=> setEmailUser(e.target.value)} value={EmailUser} /> </div>
             </div>
-
-
-
-
             <div className="form2">
               <p> Téléphone </p>
               <div className="inputField">  <input type="text" onChange={(e)=> setTelUser(e.target.value)} value={TelUser}/> </div>
             </div>
-
             <div className="form2">
               <p> Nom de l'entreprise  </p>
               <div className="inputField">  <input type="text"  onChange={(e)=> setCorporateUser(e.target.value)} value={CorporateUser}/> </div>
             </div>
-
             <div className="form2">
               <p> Secteur d'activité  </p>
               <div className="inputField">  <input type="text" onChange={(e)=> setActivityUser(e.target.value)} value={ActivityUser} /> </div>
             </div>
-
             <div className="form2">
               <p> Votre poste  </p>
               <div className="inputField">  <input type="text" onChange={(e)=> setJobUser(e.target.value)} value={JobUser} /> </div>
             </div>
-
             <div className="form2">
               <p> Genre de site web  souhaité  </p>
               <div className="inputField">  <input type="text"  onChange={(e)=> setTypeWebSiteUser(e.target.value)} value={TypeWebSiteUser}/> </div>
             </div>
-
             <div className="form2">
               <button type="submit" onClick={(e) => handleUserInfo(e)}> ENVOYER </button>
             </div>
           </div>
-
         </div>
-
-
-
       </div>
 
       <div className="footer">
@@ -161,10 +113,7 @@ const [users, setUsers] = useState([""]);
         <p> ©  2021 Agence de publicité digitale</p>
         </div>
       </div>
-
-
     </div>
   );
 }
-
 export default App;
